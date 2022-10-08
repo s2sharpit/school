51c1cea3565b93a76997d0bc1ee7c0afabeb3ceb
Tushar Saini <s2sharpit@gmail.com>
2022-10-08 20:25:59 +0530

    updated deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 import LeftBar from "./components/LeftBar"
 import barData from "./data/leftBar"
 import Footer from "./components/Footer"
@@ -17,7 +17,7 @@ export default () => {
   // const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <div className="drop-shadow-none bg-blue-50 flex flex-col lg:flex-row justify-between p-10 gap-14">
         <div className="order-2 lg:order-none flex flex-col sm:flex-row lg:flex-col gap-14 mx-auto">
@@ -35,6 +35,6 @@ export default () => {
         </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
