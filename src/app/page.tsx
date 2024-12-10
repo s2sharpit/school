3a7097cc55a3681ce3950fe931fac671eeea8fd8
Tushar Saini <s2sharpit@gmail.com>
2024-12-10 10:26:49 +0530

    chore: adding name as figcation in director & principal msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import aboutSchool from "../data/aboutSchool"
+import aboutSchool from "@/data/aboutSchool"
 
 
 export default function Home() {
@@ -13,14 +13,17 @@ export default function Home() {
   )
 }
 
-
+// aboutSchool, mission, director's message, principal's message
 function AboutSchool({ data }: {data: any}) {
   return (
       <div className="w-full group">
           <h3 className="">{data.title}</h3>
           <div>
               {(data.img) && (
-                  <Image src={"/aboutSch/" + data.img} width={100} height={100} alt="" className="w-28 float-left mr-5 rounded-md bg-white drop-shadow-2xl p-1" />
+                <figure className='float-left mr-5 w-28 rounded-md bg-white drop-shadow-2xl p-1'>
+                  <Image src={"/aboutSch/" + data.img} width={100} height={100} alt="" className="w-full" />
+                  <figcaption className='text-center text-xs mt-1 font-medium'>{data.name}</figcaption>
+                </figure>
               )}
               <p className="">
                   {(data.bold) && <b className="text-lg">{data.bold}</b>}
--- src/data/aboutSchool.js
+++ src/data/aboutSchool.js
@@ -12,11 +12,13 @@ const aboutSchool = [
     {
         title: "Director's Message",
         img: "director.jpeg",
+        name: "Sh. Prempal",
         content: "संस्कारित शिक्षा की संभावनाओं में राह तलाशना अनेक समस्याओं के समाधान को संभव बनाता है। मैं इसी विश्वास को लेकर अग्रसर हूँ। शिक्षा के क्षेत्र में उच्च जीवन-मूल्यों, महत्वाकांक्षाओं, आस्थाओं तथा संकल्पों का होना अनिवार्य है। जब हम सार्वजनिक जीवन में उतरते हैं, तो सपने एकल नहीं, बल्कि सामाजिक हो जाते हैं। जब हम इस बात को जीवन में आत्मसात् कर आगे बढ़ते हैं, तो चुनौतियाँ आसान हो जाती हैं।",
     },
     {
         title: "Principal's Message",
         img: "principal.jpeg",
+        name: "Smt. Shashi Bala",
         content: "B. S. Senior Secondary School aims at nurturing holistic development of its students towards enriching the quality of life in every aspect of well being. A co-educational, day-boarding school aiming for highest standards of academic achivement, intellectual growth, ethical awareness and behaviour, sportsmanship and other good qualities to the students has already acquired name and fame within a short span of time.",
     },
     // {
