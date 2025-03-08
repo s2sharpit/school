9e23eafe6b07676e4b5a9901be5b0ac5302952f2
Tushar Saini <s2sharpit@gmail.com>
2025-03-08 10:24:52 +0530

    feat: adding shadcnui

---
+++ src/components/ui/Title.tsx
@@ -0,0 +1,38 @@
+import { HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const titleVariants = cva("text-primary font-bold", {
+  variants: {
+    size: {
+      default: "text-3xl md:text-4xl text-center",
+      lg: "text-4xl sm:text-5xl lg:text-7xl",
+      sm: "text-2xl md:text-3xl lg:text4xl",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+interface TitleProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof titleVariants> {}
+
+const Title = forwardRef<HTMLHeadingElement, TitleProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <h1
+        ref={ref}
+        {...props}
+        className={cn(titleVariants({ size, className }))}
+      >
+        {children}
+      </h1>
+    );
+  }
+);
+
+Title.displayName = "Title";
+
+export default Title;
---
+++ src/components/ui/Wrapper.tsx
@@ -0,0 +1,40 @@
+import { HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const wrapperVariants = cva(
+  "mt-16 mx-auto gap-6 gap-x-8 md:gap-x-16 justify-center w-full",
+  {
+    variants: {
+      variant: {
+        default: "grid grid-cols-1 place-items-center",
+        flex: "flex flex-wrap md:gap-8",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface WrapperProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof wrapperVariants> {}
+
+const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(
+  ({ className, variant, children, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn(wrapperVariants({ variant, className }))}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Wrapper.displayName = "Wrapper";
+
+export default Wrapper;
